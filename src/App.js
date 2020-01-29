e808e2abd183e961284d182867d10a8f9b3012fc
anurag gharat <anuraggharat55@gmail.com>
2020-01-29 22:58:18 +0530

    Basic Boiler Plate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "weather-icons/css/weather-icon.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY ='b51f52031293aa958e4b47c75ea3ef16';
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={};
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+      hii
+      </div>
+    );
+  }
 }
 
 export default App;
