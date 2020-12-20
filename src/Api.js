1bfa24fbb8b67652eac43ae008c16b8f75f71dc9
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-12-21 00:06:34 +0530

    design done

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 export const getWeather = async () => {
-  const data = await axios.get(
-    `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_KEY}&query=Mumbai`
-  );
-  console.log(data);
+  try {
+    const res = await axios.get(
+      `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_KEY}&query=Mumbai`
+    );
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return { status: "error", error };
+  }
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { getWeather } from "./Api";
 
 export default function App() {
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [location, setLocation] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const fetchCurrentWeather = () => {
+    setLoading(true);
+    setError("");
+    try {
+      getWeather()
+        .then((res) => setCurrentWeather(res.current))
+        .then((res) => setLocation(res.location))
+        .catch((err) => setError("Something went wrong"))
+        .finally(() => setLoading(false));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(currentWeather.weather_icons);
+  console.log(currentWeather);
   useEffect(() => {
-    getWeather();
+    fetchCurrentWeather();
   }, []);
 
-  return <div className="app">{process.env.REACT_APP_WEATHER_KEY}</div>;
+  return (
+    <div className="app">
+      {!loading && (
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <p>Friday 26th December</p>
+              <h3>{location.name}</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h1>27&deg; C</h1>
+            </div>
+            <div className="col">
+              <h3>HAZE</h3>
+              <div className="row-2">
+                <div className="card">
+                  <p>Wind Speed</p>
+                  <h2>23Kmph</h2>
+                </div>
+                <div className="card">
+                  <p>Wind Speed</p>
+                  <h2>23Kmph</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
