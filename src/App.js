91a9bb7f854588c02e3cca304f8553987e8368a2
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-12-22 20:35:28 +0530

    final product

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ export default function App() {
   const [location, setLocation] = useState({});
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
+  var d = new Date();
+
   const fetchCurrentWeather = () => {
     setLoading(true);
     setError("");
     try {
       getWeather()
-        .then((res) => setCurrentWeather(res.current))
-        .then((res) => setLocation(res.location))
+        .then((res) => {
+          setCurrentWeather(res.current);
+          setLocation(res.location);
+        })
         .catch((err) => setError("Something went wrong"))
         .finally(() => setLoading(false));
     } catch (error) {
@@ -21,11 +26,11 @@ export default function App() {
     }
   };
 
-  console.log(currentWeather.weather_icons);
-  console.log(currentWeather);
   useEffect(() => {
     fetchCurrentWeather();
   }, []);
+
+  console.log();
 
   return (
     <div className="app">
@@ -33,24 +38,27 @@ export default function App() {
         <div className="container">
           <div className="row">
             <div className="col">
-              <p>Friday 26th December</p>
+              <p>{d.toDateString()}</p>
+
               <h3>{location.name}</h3>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <h1>27&deg; C</h1>
+              <h1>
+                {currentWeather.temperature}&deg;<span>C</span>
+              </h1>
             </div>
             <div className="col">
-              <h3>HAZE</h3>
+              <h3>{currentWeather.weather_descriptions}</h3>
               <div className="row-2">
                 <div className="card">
                   <p>Wind Speed</p>
-                  <h2>23Kmph</h2>
+                  <h2>{currentWeather.wind_speed}</h2>
                 </div>
                 <div className="card">
-                  <p>Wind Speed</p>
-                  <h2>23Kmph</h2>
+                  <p>Humidity</p>
+                  <h2>{currentWeather.humidity}</h2>
                 </div>
               </div>
             </div>
